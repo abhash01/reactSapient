ccf2b6474c9f63bd13df7cbd59fc661e823393af
Abhash kumar ranjan <32848595+abhash01@users.noreply.github.com>
2020-09-14 01:06:26 +0530

    Updates

---
+++ precache-manifest.7a79a7e4f2085efb1d4607e2bddc1c27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9d130a86575a78364f3e00b1a2633bd",
+    "url": "/abhash01/reactSapient.git/index.html"
+  },
+  {
+    "revision": "1814840a94ac57ffb0c1",
+    "url": "/abhash01/reactSapient.git/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "51ae4d2f864992019cfb",
+    "url": "/abhash01/reactSapient.git/static/css/main.3a7931ec.chunk.css"
+  },
+  {
+    "revision": "1814840a94ac57ffb0c1",
+    "url": "/abhash01/reactSapient.git/static/js/2.6154f00c.chunk.js"
+  },
+  {
+    "revision": "9e81e617707513432f3da640fbf3bd82",
+    "url": "/abhash01/reactSapient.git/static/js/2.6154f00c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51ae4d2f864992019cfb",
+    "url": "/abhash01/reactSapient.git/static/js/main.33482617.chunk.js"
+  },
+  {
+    "revision": "d4b31887ba7d3b87e341",
+    "url": "/abhash01/reactSapient.git/static/js/runtime-main.99ee4a25.js"
+  }
+]);
